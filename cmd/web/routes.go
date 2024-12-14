e2a736cf627cfc7888b963e15bb756ed4f2e9023
kons77 <staneua@gmail.com>
2024-12-14 19:52:51 +0200

    Adding images and static

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/kons77/room-bookings-app/pkg/config"
 	"github.com/kons77/room-bookings-app/pkg/handlers"
 
@@ -18,6 +19,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// tell our app where to find static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
