5b60a050f51d33680ec9ccd03020773c55753d4f
kons77 <staneua@gmail.com>
2024-12-29 01:00:24 +0200

    Added response page and alerts

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,6 +9,7 @@ import (
 
 	"github.com/kons77/room-bookings-app/internal/config"
 	"github.com/kons77/room-bookings-app/internal/handlers"
+	"github.com/kons77/room-bookings-app/internal/models"
 	"github.com/kons77/room-bookings-app/internal/render"
 
 	"github.com/alexedwards/scs/v2"
@@ -18,8 +20,10 @@ const portNumber = ":8080"
 var app config.AppConfig
 var session *scs.SessionManager
 
-// main os the main application function
+// main is the main application function
 func main() {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
 
 	//change this ti true when in production
 	app.InProduction = false
@@ -43,7 +47,7 @@ func main() {
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 
-	fmt.Printf("Starting application on port %s", portNumber)
+	fmt.Printf("Starting application on port %s \n", portNumber)
 
 	srv := &http.Server{
 		Addr:    portNumber,
