8fc6606fd7e094c122a6c0e8390cfbc1357cc905
kons77 <staneua@gmail.com>
2025-01-29 23:21:25 +0200

    bootstrap templates & handle reservation calendar

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -662,6 +662,9 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 		now = time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
 	}
 
+	data := make(map[string]interface{})
+	data["now"] = now
+
 	next := now.AddDate(0, 1, 0)
 	last := now.AddDate(0, -1, 0)
 
@@ -680,8 +683,30 @@ func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Re
 	stringMap["this_month"] = now.Format("01")
 	stringMap["this_month_year"] = now.Format("2006")
 
+	// get the first and the last days of the month
+	currentYear, currentMonth, _ := now.Date()
+	currentLocation := time.UTC //now.Location()
+	firstOfMonth := time.Date(currentYear, currentMonth, 1, 0, 0, 0, 0, currentLocation)
+	lastOfMonth := firstOfMonth.AddDate(0, 1, -1) // + no years, 1 month and -1 day
+
+	// days in the current month
+	intMap := make(map[string]int)
+	intMap["days_in_month"] = lastOfMonth.Day()
+	log.Println(intMap)
+
+	// get all rooms info
+	rooms, err := m.DB.AllRooms()
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	data["rooms"] = rooms
+
 	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
+		Data:      data,
+		IntMap:    intMap,
 	})
 }
 
--- internal/render/render.go
+++ internal/render/render.go
@@ -16,11 +16,30 @@ import (
 
 // holds all of the functions that we want to put into or make available to Goland templates.
 var functions = template.FuncMap{
-	"humanDate": HumanDate,
+	"humanDate":  HumanDate,
+	"formatDate": FormatDate,
+	"iterate":    Iterate,
+	// "add": Add,
 }
 
 var app *config.AppConfig
 var pathToTemplates = "./templates"
+
+/*
+func Add(a,b int) int{
+	return a+b
+}
+*/
+
+// Iterate returns a slice of ints starting at 1, going to count
+func Iterate(count int) []int {
+	var i int
+	var items []int
+	for i = 1; i < count; i++ {
+		items = append(items, i)
+	}
+	return items
+}
 
 // NewRenderer sets the config for the template package
 func NewRenderer(a *config.AppConfig) {
@@ -30,6 +49,11 @@ func NewRenderer(a *config.AppConfig) {
 // HumanDate returns time in YYYY-MM-DD format
 func HumanDate(t time.Time) string {
 	return t.Format("2006-01-02")
+}
+
+// FormatDate shows date as a month or as a year
+func FormatDate(t time.Time, f string) string {
+	return t.Format(f)
 }
 
 // AddDefaultData adds data for all templates (displayed on every page)
